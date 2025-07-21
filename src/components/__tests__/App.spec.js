6d123cb832b2f014562d90e9365a51f84033a398
Mehdi Bellam <porcepic91@gmail.com>
2025-07-21 11:41:38 +0200

    Fail teste Unitaire

--- src/components/__tests__/App.spec.js
+++ src/components/__tests__/App.spec.js
@@ -17,9 +17,8 @@ describe('App.vue', () => {
     const wrapper = mount(App)
     expect(wrapper.find('button.neon-btn').exists()).toBe(true)
   })
-  // eslint-disable-next-line vitest/no-commented-out-tests
-  //   it('Le boutton n est pas activé', () => {
-  //     const wrapper = mount(App)
-  //     expect(wrapper.find('button.neon-btn').attributes('disabled')).toBeDefined()
-  //   })
+  it('Le boutton n est pas activé', () => {
+    const wrapper = mount(App)
+    expect(wrapper.find('button.neon-btn').attributes('disabled')).toBeDefined()
+  })
 })
